89b27a4ebf2d589ea25181677b9540ace43e6865
Mattyu <ememattyu12@gmail.com>
2021-05-07 07:38:48 -0600

    Reset

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
-const sec = document.querySelector('.sec');
-const btn = document.querySelector('.btn-dark');
-btn.onclick = function(){
-    sec.classList.btn('dark');
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+
+    if (document.body.classList.contains('dark')) {
+        localStorage.setItem('dark-mode', 'true');
+    } else {
+        localStorage.setItem('dark-mode', 'false');
+    }
+});
+
+if (localStorage.getItem('dark-mode') === 'true') {
+    document.body.classList.add('dark');
+    btnSwitch.classList.add('active');
+} else {
+    document.body.classList.remove('dark');
+    btnSwitch.classList.remove('active');
 }
