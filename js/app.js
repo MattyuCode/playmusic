c30d866896719a49737385ed2284114e6f8149dd
Mattyu <ememattyu12@gmail.com>
2021-05-09 22:28:06 -0600

    All

--- js/app.js
+++ js/app.js
@@ -1,20 +1,70 @@
-const btnSwitch = document.querySelector('#switch');
+// boton de dark & ligth
+const Body = document.querySelector('body');
+const BotonToogleTheme = document.querySelector('.toogle-theme');
 
-btnSwitch.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
-    btnSwitch.classList.toggle('active');
+BotonToogleTheme.onclick = () => {
+    Body.classList.toggle('dark');
 
-    if (document.body.classList.contains('dark')) {
-        localStorage.setItem('dark-mode', 'true');
+    if (Body.classList.contains('dark')) {
+        localStorage.setItem('ModoDarkAgregado', 'true');
     } else {
-        localStorage.setItem('dark-mode', 'false');
+        localStorage.setItem('ModoDarkAgregado', 'false');
     }
-});
+}
 
-if (localStorage.getItem('dark-mode') === 'true') {
-    document.body.classList.add('dark');
-    btnSwitch.classList.add('active');
+if (localStorage.getItem('ModoDarkAgregado') === 'true') {
+    Body.classList.add('dark');
 } else {
-    document.body.classList.remove('dark');
-    btnSwitch.classList.remove('active');
+    Body.classList.remove('dark');
+}
+
+/*-------------------------------------------
+        code de listado de musicas
+  -------------------------------------------
+*/
+
+const wrApper = document.querySelector('.wrapper'),
+    musicImg = wrApper.querySelector('.image_area img'),
+    musicName = wrApper.querySelector('.song_details .name'),
+    musicArtist = wrApper.querySelector('.song_details  .artist'),
+    musicAudio = wrApper.querySelector('#audioSong'),
+    playPauseBtn = wrApper.querySelector('.play-pause');
+
+
+let musicIndex = 10;
+
+window.addEventListener('load', () => {
+    loadMusic(musicIndex);
+})
+
+// función de cargar música
+function loadMusic(indexNumb) {
+    musicName.innerHTML = listMusicaAll[indexNumb - 1].name;
+    musicArtist.innerHTML = listMusicaAll[indexNumb - 1].artist;
+    musicImg.src = `img/${listMusicaAll[indexNumb - 1].img}.jpg`;
+    musicAudio.src = `music/${listMusicaAll[indexNumb - 1].src}.mp3`;
+}
+
+// función de reproducción música
+function playMusic() {
+    wrApper.classList.add('paused');
+    playPauseBtn.querySelector('i').innerText = 'pause';
+    musicAudio.play();
+}
+
+// función de pausar música
+function pauseMusic() {
+    wrApper.classList.remove('paused');
+    playPauseBtn.querySelector('i').innerText = 'play_circle';
+
+    musicAudio.pause();
+}
+
+
+playPauseBtn.addEventListener('click', () => {
+    const isMusicPause = wrApper.classList.contains('paused');
+    isMusicPause ? pauseMusic() : playMusic();
+});
+ {
+    /* <i class="material-icons">play_circle</i> */
 }
---
+++ js/listMusic.js
@@ -0,0 +1,124 @@
+// code de listado de musicas
+
+let listMusicaAll = [{
+        name: 'Isla Morenita',
+        artist: 'Carlos Sadness',
+        img: 'TropicalJesus',
+        src: 'Isla Morenita',
+        number: 1
+    },
+    {
+        name: 'Groenlandia',
+        artist: 'Carlos Sadness',
+        img: 'groenlandia',
+        src: 'Groenlandia',
+        number: 2
+    },
+    {
+        name: 'Noche de Reyes',
+        artist: 'Carlos Sadness',
+        img: 'noche',
+        src: 'Noche de Reyes',
+        number: 3
+    },
+    {
+        name: 'Poli (Zoe Cover)',
+        artist: 'Carlos Sadness',
+        img: 'noche',
+        src: 'Poli (Zoe Cover)',
+        number: 4
+    },
+    {
+        name: 'Volcanes Dormidos',
+        artist: 'Carlos Sadness',
+        img: 'VolcanesDormidos',
+        src: 'Volcanes Dormidos',
+        number: 5
+    },
+    {
+        name: 'Kandinski',
+        artist: 'Carlos Sadness',
+        img: 'DiferentesTiposdeLuz',
+        src: 'Kandinski',
+        number: 6
+    },
+    {
+        name: 'Amor Papaya (Ft. Caloncho)',
+        artist: 'Carlos Sadness',
+        img: 'DiferentesTiposdeLuz',
+        src: 'Amor Papaya (Ft. Caloncho)',
+        number: 7
+    },
+    {
+        name: 'Chihuahua',
+        artist: 'Carlos Sadness',
+        img: 'DiferentesTiposdeLuz',
+        src: 'Chihuahua',
+        number: 8
+    },
+    {
+        name: 'Te Quiero un Poco',
+        artist: 'Carlos Sadness',
+        img: 'DiferentesTiposdeLuz',
+        src: 'Te Quiero un Poco',
+        number: 9
+    },
+    {
+        name: 'Semitransparente',
+        artist: 'Carlos Sadness',
+        img: 'DiferentesTiposdeLuz',
+        src: 'Semitransparente',
+        number: 10
+    },
+    {
+        name: 'Silencio Antiguo',
+        artist: 'Carlos Sadness',
+        img: 'DiferentesTiposdeLuz',
+        src: 'Silencio Antiguo',
+        number: 11
+    },
+    {
+        name: 'Sebastian Bach',
+        artist: 'Carlos Sadness',
+        img: 'DiferentesTiposdeLuz',
+        src: 'Sebastian Bach',
+        number: 12
+    },
+    {
+        name: 'Pompeia',
+        artist: 'Carlos Sadness',
+        img: 'DiferentesTiposdeLuz',
+        src: 'Pompeia',
+        number: 13
+    },
+
+    {
+        name: 'Longitud de Onda',
+        artist: 'Carlos Sadness',
+        img: 'DiferentesTiposdeLuz',
+        src: 'Longitud de Onda',
+        number: 14
+    },
+    {
+        name: 'Hale Bopp',
+        artist: 'Carlos Sadness',
+        img: 'DiferentesTiposdeLuz',
+        src: 'Hale Bopp',
+        number: 15
+    },
+    {
+        name: 'Física Moderna',
+        artist: 'Carlos Sadness',
+        img: 'DiferentesTiposdeLuz',
+        src: 'Física Moderna',
+        number: 16
+    },
+    {
+        name: 'El Relámpago',
+        artist: 'Carlos Sadness',
+        img: 'DiferentesTiposdeLuz',
+        src: 'El Relámpago',
+        number: 17
+    }
+
+]
